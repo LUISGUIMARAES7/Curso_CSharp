e03ad10930563070b468cf3ee5e10c426c7270b0
LUIS FERNANDO GUIMARAES <luis.fgsantos6@senacsp.edu.br>
2025-01-16 21:06:44 -0300

    Atualização do exercicio

---
+++ Exx007/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exx007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota_final = 60;
+            string resultado;
+
+            if (nota_final >= 60)
+            {
+                resultado = "Aprovado";
+            }
+            else if (nota_final >= 40 & nota_final < 60)
+            {
+                resultado = "Recuperação";
+            }
+            else
+            {
+                resultado = "Reprovado";
+            }
+
+
+            Console.WriteLine("O aluno está {0}.", resultado);
+
+        }
+    }
+}
