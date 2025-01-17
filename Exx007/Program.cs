84ff7aae2b8867690824a72ab89a3426f9702f8d
LUIS FERNANDO GUIMARAES <luis.fgsantos6@senacsp.edu.br>
2025-01-16 22:11:11 -0300

    Atualição do exercício

--- Exx007/Program.cs
+++ Exx007/Program.cs
@@ -4,12 +4,26 @@
     {
         static void Main(string[] args)
         {
-            double nota_final = 95;
+            double nota_final, n1,n2,n3,n4;
             string resultado;
+            
+            Console.Write("Digite a primeira nota do aluno: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
 
-            if (nota_final >= 60)
-            { 
-                if (nota_final > 90) 
+            Console.Write("Digite a segunda nota do aluno: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a terceira nota do aluno: ");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a quarta nota do aluno: ");
+            n4 = Convert.ToDouble(Console.ReadLine());
+
+            nota_final = (n1+n2+n3+n4) / 4;
+
+            if (nota_final >= 7)
+            {
+                if (nota_final > 10)
                 {
                     resultado = "Aprovado com louvor";
                 }
@@ -20,7 +34,7 @@
             }
             else
             {
-                if (nota_final >= 40)
+                if (nota_final >= 5)
                 {
                     resultado = "Recuperação";
                 }
@@ -31,8 +45,10 @@
             }
 
 
-            Console.WriteLine(" Nota do aluno: {0} - Situação: {1}.", nota_final, resultado);
-
+            Console.WriteLine("Nota do aluno: {0} - Situação: {1}.", nota_final, resultado);
+                
+            }
         }
     }
-}
+
+
