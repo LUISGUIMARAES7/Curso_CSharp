db5c8b2918c3d3d02d5db41a78251ceeddd25e99
LUIS FERNANDO GUIMARAES <luis.fgsantos6@senacsp.edu.br>
2025-01-21 19:58:19 -0300

    Exercicio triângulo

--- Ex009/Program.cs
+++ Ex009/Program.cs
@@ -6,30 +6,41 @@
         {
             int ladoA, ladoB, ladoC;
 
-            Console.Write("Digite o valor do lado A: ");
-            ladoA = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Digite o valor do lado B: ");
-            ladoB = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Digite o valor do lado C: ");
-            ladoC = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
 
-            if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoB))
-            {
-                Console.WriteLine("O valores informados formam um triângulo.");
-            }
-            else 
-            {
-                Console.WriteLine("O valores informados não formam um triângulo.");
+                Console.Write("Digite o valor do lado A: ");
+                ladoA = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Digite o valor do lado B: ");
+                ladoB = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Digite o valor do lado C: ");
+                ladoC = Convert.ToInt32(Console.ReadLine());
+
+                if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoB))
+                {
+                    Console.Write("O valores informados formam um triângulo: ");
+
+                    if (ladoA == ladoB && ladoB == ladoC)
+                    {
+                        Console.WriteLine("Equilátero");
+                    }
+                    else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+                    {
+                        Console.WriteLine("Isósceles");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Escaleno");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("O valores informados não formam um triângulo.");
+                }
+
+                Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
             }
 
-            if (ladoA == ladoB && ladoB == ladoC)
-            {
-                Console.WriteLine("O triângulo é equilátero");
-            }
-            else if (ladoA == ladoB)
-            {
-                
-            }
         }               
     }
 }
