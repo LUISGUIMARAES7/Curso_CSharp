67a1b2a6055f8d4134a55ea826f3fbdc4016a927
LUIS FERNANDO GUIMARAES <luis.fgsantos6@senacsp.edu.br>
2025-01-21 19:31:20 -0300

    exercicios

---
+++ Ex008/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            while (true)
+            {
+
+                Console.Write("Digite um número: ");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine("o número {0} é par", num);
+                }
+                else
+                {
+                    Console.WriteLine("o número {0} é ímpar", num);
+                }
+
+                Console.WriteLine("--------------------");
+            }
+        }
+    }
+}
---
+++ Ex009/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Ex009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ladoA, ladoB, ladoC;
+
+            Console.Write("Digite o valor do lado A: ");
+            ladoA = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o valor do lado B: ");
+            ladoB = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o valor do lado C: ");
+            ladoC = Convert.ToInt32(Console.ReadLine());
+
+            if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoB))
+            {
+                Console.WriteLine("O valores informados formam um triângulo.");
+            }
+            else 
+            {
+                Console.WriteLine("O valores informados não formam um triângulo.");
+            }
+
+            if (ladoA == ladoB && ladoB == ladoC)
+            {
+                Console.WriteLine("O triângulo é equilátero");
+            }
+            else if (ladoA == ladoB)
+            {
+                
+            }
+        }               
+    }
+}
