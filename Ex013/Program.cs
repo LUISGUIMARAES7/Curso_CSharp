350896da4e2be42348d4007ab562435996d04321
LUIS FERNANDO GUIMARAES <luis.fgsantos6@senacsp.edu.br>
2025-01-21 22:25:58 -0300

    exercicio 13

---
+++ Ex013/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ex013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoAtual = 2024;
+            int anoNasc, idade;
+
+            Console.WriteLine("Digite seu ano de nascimento");
+            anoNasc = Convert.ToInt32(Console.ReadLine());
+
+            idade = anoAtual - anoNasc;
+
+            if (idade < 18)
+            {
+                Console.WriteLine("Você ainda não tem idade para se alistar");
+                Console.WriteLine("Faltam {0} anos", 18 - idade);
+
+            }
+            else if (idade == 18)
+            {
+                Console.WriteLine("Parabéns, você tem 18 anos e está na hora de se alistar!");
+            }
+            else 
+            {
+                Console.WriteLine("Você");
+            }
+        }
+    }
+}
