d3648d01cac5534f382a80f0a10ae5c7e824c653
LUIS FERNANDO GUIMARAES <luis.fgsantos6@senacsp.edu.br>
2025-01-21 22:15:30 -0300

    exercicio 12

--- Ex012/Program.cs
+++ Ex012/Program.cs
@@ -4,8 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Digite um número inteiro: ");
-            Console.Write("Digite um número inteiro: ");
+            int n1, n2;
+
+            Console.Write("Digite o primeiro valor: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                Console.WriteLine("O primeiro valor é maior.");
+            }
+            else if (n1 < n2) 
+            {
+                 Console.WriteLine("O segundo valor é maior.");
+            }
+            else
+            {
+                Console.WriteLine("Não existe valor maior, os dois são iguais.");
+            }
+
         }
     }
 }
