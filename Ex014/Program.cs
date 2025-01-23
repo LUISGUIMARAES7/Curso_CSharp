56f074548bd0c487c2190bea027d7202d8026e2e
LUIS FERNANDO GUIMARAES <luis.fgsantos6@senacsp.edu.br>
2025-01-23 19:04:23 -0300

    exerciocios

---
+++ Ex014/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ex014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++) 
+            {
+                Console.WriteLine("Hello, World!");
+            }
+
+            
+        }
+    }
+}
