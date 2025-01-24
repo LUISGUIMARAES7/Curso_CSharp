1ec724be66ff8cc9e8d261f16ae2ac9026fe7745
LUIS FERNANDO GUIMARAES <luis.fgsantos6@senacsp.edu.br>
2025-01-23 21:14:30 -0300

    exercicios

---
+++ Ex024/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, soma, qtd;
+            soma = qtd = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Digite um valor");
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n % 2 == 0)
+                {
+                    soma += n;
+                    qtd++;
+                }
+            }
+            Console.WriteLine("A soma dos {0} valores pares é {1}",qtd, soma);
+        }
+    }
+}
