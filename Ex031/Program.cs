453adfa292d172bc3a1de6d22172c296b7286b16
LUIS FERNANDO GUIMARAES <luis.fgsantos6@senacsp.edu.br>
2025-01-28 19:48:37 -0300

    exercicio

--- Ex031/Program.cs
+++ Ex031/Program.cs
@@ -4,16 +4,40 @@
     {
         static void Main(string[] args)
         {
-            string senha = "123";
+            string senha = "123"; 
             string senhaDigitada;
+            int tentativas = 0;
 
-            Console.WriteLine("Digite a senha");
-            senhaDigitada = Console.ReadLine();
-
-            while (senhaDigitada == senha)
+            do
             {
+                Console.Clear();   
+                Console.Write("Digite a senha: ");
+                senhaDigitada = Console.ReadLine();
+                tentativas++;
+                if ( senhaDigitada != senha & tentativas < 3)
+                {
+                    Console.WriteLine("Senha incorreta");
+                    Console.WriteLine("Você ainda tem " + (3 - tentativas) + " tentativas");
+                    Console.ReadKey();
+                } 
+                else if ( senhaDigitada == senha)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Senha correta! - Tentativas: " + tentativas);
+                    return;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Senha incorreta. Seu acesso está bloqueado.");
+                    return;
+                }
+            } while (senhaDigitada != senha);
 
-            }
+            
+            
+
+           
         }
     }
 }
