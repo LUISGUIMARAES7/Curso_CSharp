a5283be4db4bdb5ac6566d59b6f63a5a7e74075b
LUIS FERNANDO GUIMARAES <luis.fgsantos6@senacsp.edu.br>
2025-01-28 18:58:47 -0300

    exercicio

---
+++ Ex031/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex031
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "123";
+            string senhaDigitada;
+
+            Console.WriteLine("Digite a senha");
+            senhaDigitada = Console.ReadLine();
+
+            while (senhaDigitada == senha)
+            {
+
+            }
+        }
+    }
+}
