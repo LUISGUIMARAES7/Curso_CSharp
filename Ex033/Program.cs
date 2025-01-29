4111120a22217c997cf3b5f3085061f21457cae0
LUIS FERNANDO GUIMARAES <luis.fgsantos6@senacsp.edu.br>
2025-01-28 22:21:57 -0300

    exercicio

---
+++ Ex033/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ex033
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] frutas = { "Maça", "Banana", "Manga", "Uva", "Pêra", "Jaca", "Laranja", "Acerola", "Goiaba", "Limão" };
+            double[] preço = { 10, 4, 5, 15, 14, 20, 6, 8, 9, 5 };
+
+            for (int i = 0; i < frutas.Length; i++)
+            {
+                Console.WriteLine("O preço da " + frutas[i] + " é R$" + preço[i]);
+            }
+             
+
+        }
+    }
+}
