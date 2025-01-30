7575a4175e9208143d855bca755fe1e3bb63ec81
LUIS FERNANDO GUIMARAES <luis.fgsantos6@senacsp.edu.br>
2025-01-30 18:54:08 -0300

    exercicios

--- Ex033/Program.cs
+++ Ex033/Program.cs
@@ -5,13 +5,17 @@
         static void Main(string[] args)
         {
             string[] frutas = { "Maça", "Banana", "Manga", "Uva", "Pêra", "Jaca", "Laranja", "Acerola", "Goiaba", "Limão" };
-            double[] preço = { 10, 4, 5, 15, 14, 20, 6, 8, 9, 5 };
+            double[] precos = { 10, 4, 5, 15, 14, 20, 6, 8, 9, 5 };
 
             for (int i = 0; i < frutas.Length; i++)
             {
-                Console.WriteLine("O preço da " + frutas[i] + " é R$" + preço[i]);
+                Console.WriteLine("O preço da " + frutas[i] + " é R$" + precos[i]);
             }
-             
+
+            Console.WriteLine();
+            Console.WriteLine("O maior preço é {0}", precos.Max());
+            Console.WriteLine("O menor preço é {0}", precos.Min());
+            Console.WriteLine("A média dos preços é {0}", precos.Average());
 
         }
     }
