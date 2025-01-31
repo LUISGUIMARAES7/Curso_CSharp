3e5eb1b8409be848c34258e4366b1d3c6ff5b5fb
LUIS FERNANDO GUIMARAES <luis.fgsantos6@senacsp.edu.br>
2025-01-30 21:31:27 -0300

    exercicio método

---
+++ AreaTriangulo/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AreaTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============================");
+            Console.WriteLine(" Calcular Área do Triângulo");
+            Console.WriteLine("============================");
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a base do triângulo (m): ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a altura do triângulo (m): ");
+            double h = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            AreaTriangulo(b, h);
+        }
+
+        static void AreaTriangulo(double b, double h)
+        {
+            double area = b * h / 2;
+            Console.WriteLine($"A área do triângulo é {area:F2}m²");
+        }
+    }
+}
