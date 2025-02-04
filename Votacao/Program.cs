b35884d85e19461acb696473a9b227481dcac6c2
LUIS FERNANDO GUIMARAES <luis.fgsantos6@senacsp.edu.br>
2025-02-04 20:19:34 -0300

    exercicio

---
+++ Votacao/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Votacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual seu ano de nascimento? ");
+            int nasc = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine(Voto(nasc));
+
+        }
+
+        static string Voto(int ano)
+        {
+            DateTime dataAtual = DateTime.Now;
+            int anoAtual = dataAtual.Year;
+
+            int idade = anoAtual - ano;
+            if (idade < 16)
+            {
+                return $"Com {idade} anos você não vota!";
+            }
+            else if (idade >= 18 && idade < 70)
+            {
+                return $"Com {idade} anos você vota!";
+            }
+            else
+            {
+                return $"Com {idade} anos seu voto é opcional!";
+            }
+        }
+    }
+}
