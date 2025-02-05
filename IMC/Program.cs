68dc9b70cd55f3eb8db2bf564f9213f8409849d2
LUIS FERNANDO GUIMARAES <luis.fgsantos6@senacsp.edu.br>
2025-02-04 21:32:28 -0300

    exercicio

---
+++ IMC/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu peso: ");
+            double peso = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite sua altura: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            double indice = peso / (altura * altura);
+
+            Console.WriteLine($"Seu IMC é {indice:F2}");
+        }
+
+    }
+}
